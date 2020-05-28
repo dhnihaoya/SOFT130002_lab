660907776e3bc45457264c1c75eb49683334a18e
dhnihaoya <19302010002@fudan.edu.cn>
2020-05-28 11:25:15 +0800

    Add files via upload

---
+++ lab9/lab9-exercise1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Exercise 1</title>
+</head>
+<body>
+<h1>Regular HTML section (outside &lt;?php ... ?&gt; tags)</h1>
+<p>You can type regular HTML here and it will show up</p>
+
+<h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
+<?php
+$date = date("l, M dS, Y , H:i:s");
+echo "This page was generated: " . $date . "<hr/>";
+$remaining = 365 + date("L");
+$remaining = 365 - date("z");
+echo "There are ". $remaining . " days left in the year";
+?>
+</body>
+</html>
